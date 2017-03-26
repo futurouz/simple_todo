4923d9a4dc85918aa0d8776288888d12078f1610
futurouz <kunanan.tas@mail.kmutt.ac.th>
2017-03-27 00:12:12 +0700

    add todo views

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ var app = express();
 app.set('view engine','ejs');
 app.use(express.static('./public'));
 
+app.get('/todo',function(req,res){
+    res.render('todo');
+});
+
 app.listen(8080);
 console.log('Server is listening to port 8080');
