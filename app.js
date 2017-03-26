5fb22a611ea9acf4bac1f2516d15a614369d4751
futurouz <kunanan.tas@mail.kmutt.ac.th>
2017-03-26 23:48:25 +0700

    first commit and add app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine','ejs');
+app.use(express.static('./public'));
+
+app.listen(8080);
+console.log('Server is listening to port 8080');
