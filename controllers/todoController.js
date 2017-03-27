611241af26b16a373a321b2a4c9ff3ca9edb1e68
futurouz <kunanan.tas@mail.kmutt.ac.th>
2017-03-27 23:24:17 +0700

    connected mongoDB and add action method to form

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,3 +1,16 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+//Connect To db
+mongoose.connect('mongodb://test:test@ds035240.mlab.com:35240/todo');
+
+var todoSchema = mongoose.Schema({
+    item: String
+});
+var Todo = mongoose.model('Todo',todoSchema);
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 var data = [{ task: 'read a book' }, { task: 'pratice english' }, { task: 'coding everyday' }];
 
 module.exports = function (app) {
@@ -5,9 +18,9 @@ module.exports = function (app) {
         res.render('todo', { todo: data });
     });
     app.post('/todo', function (req, res) {
-
-    });
+        // do something
+    }); 
     app.delete('/todo/:item', function (req, res) {
-
+        // do something
     });
 };
