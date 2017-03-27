233271ccdd2ba766476557d6133644f7ea8a7f63
futurouz <kunanan.tas@mail.kmutt.ac.th>
2017-03-28 00:02:12 +0700

    add get and post in todoController

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,25 +2,31 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //Connect To db
-mongoose.connect('mongodb://test:test@ds035240.mlab.com:35240/todo');
+mongoose.connect('mongodb://test:test@ds143330.mlab.com:43330/todo2');
 
 var todoSchema = mongoose.Schema({
     item: String
 });
-var Todo = mongoose.model('Todo',todoSchema);
+var Todo = mongoose.model('Todo', todoSchema);
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-var data = [{ task: 'read a book' }, { task: 'pratice english' }, { task: 'coding everyday' }];
+//var data = [{ task: 'read a book' }, { task: 'pratice english' }, { task: 'coding everyday' }];
 
 module.exports = function (app) {
     app.get('/todo', function (req, res) {
-        res.render('todo', { todo: data });
+        Todo.find({}, function (err, data) {
+            if (err) throw err;
+            res.render('todo', { todo: data });
+        });
     });
-    app.post('/todo', function (req, res) {
-        // do something
-    }); 
-    app.delete('/todo/:item', function (req, res) {
-        // do something
-    });
+    app.post('/todo', urlencodedParser, function (req, res) {
+        var newTodo = new Todo(req.body).save(function (err, data) {
+            if (err) throw err;
+            res.redirect('back');
+       });
+});
+app.delete('/todo/:item', function (req, res) {
+    // do something
+});
 };
