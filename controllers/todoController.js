e25d5e2a9adca924382ba2968eb9b7389cba00c2
futurouz <kunanan.tas@mail.kmutt.ac.th>
2017-03-28 00:19:44 +0700

    fixed show task from lastest

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,6 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-//Connect To db
 mongoose.connect('mongodb://test:test@ds143330.mlab.com:43330/todo2');
 
 var todoSchema = mongoose.Schema({
@@ -24,9 +23,9 @@ module.exports = function (app) {
         var newTodo = new Todo(req.body).save(function (err, data) {
             if (err) throw err;
             res.redirect('back');
-       });
-});
-app.delete('/todo/:item', function (req, res) {
-    // do something
-});
+        });
+    });
+    app.delete('/todo/:item', function (req, res) {
+        // do something
+    });
 };
