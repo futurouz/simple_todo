aa5d9fc8743980f509f22f3117762093721da32b
futurouz <kunanan.tas@mail.kmutt.ac.th>
2017-04-08 12:03:04 +0700

    add Env Port

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ app.use(express.static('./public'));
 
 todoController(app);
 
-app.listen(3030);
-console.log('Server is listening to port 3030');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
